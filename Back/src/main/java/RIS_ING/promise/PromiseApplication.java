ba4687da41166309c9833a6ef616107c50e273db
Lee SeungWook <leesw1998@gmail.com>
2024-07-03 07:53:15 +0900

    기본 폴더, 스프링 부트 3.3.1 init

---
+++ Back/src/main/java/RIS_ING/promise/PromiseApplication.java
@@ -0,0 +1,13 @@
+package RIS_ING.promise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromiseApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/RIS_ING/promise/PromiseApplicationTests.java
@@ -0,0 +1,13 @@
+package RIS_ING.promise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
