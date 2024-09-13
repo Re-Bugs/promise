0c0feb444021101ea6f86b47704d72531116ae78
SeungWook_Lee <leesw_0924@icloud.com>
2024-09-13 17:53:05 +0900

    SpringBoot Project - init

---
+++ SpringBoot/src/main/java/com/promise/promise/PromiseApplication.java
@@ -0,0 +1,12 @@
+package com.promise.promise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class PromiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromiseApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/src/test/java/com/promise/promise/PromiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promise.promise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
