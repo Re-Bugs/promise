1dbaa260ac8bbea1ff371330b19c270f349fd6a4
SeungWook_Lee <leesw_0924@icloud.com>
2024-09-20 11:43:05 +0900

    timezone test3

--- SpringBoot/src/main/java/com/promise/promise/PromiseApplication.java
+++ SpringBoot/src/main/java/com/promise/promise/PromiseApplication.java
@@ -1,12 +1,22 @@
 package com.promise.promise;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PromiseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PromiseApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// JVM의 기본 시간대를 Asia/Seoul로 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
