32f2e0f1ec42c67198020c3d0213c38773dfb530
SeungWook_Lee <leesw_0924@icloud.com>
2024-10-14 05:08:32 +0900

    백엔드 - 로그 파일 순서 정렬, 전체 복약 순응도 계산 로직 개선

--- SpringBoot/src/main/java/com/onlypromise/promise/controller/web/admin/adminHomeController.java
+++ SpringBoot/src/main/java/com/onlypromise/promise/controller/web/admin/adminHomeController.java
@@ -4,6 +4,7 @@ import com.onlypromise.promise.DTO.web.AdminHomeDTO;
 import com.onlypromise.promise.DTO.web.reportAdminDTO;
 import com.onlypromise.promise.domain.Report;
 import com.onlypromise.promise.domain.User;
+import com.onlypromise.promise.domain.enumeration.Role;
 import com.onlypromise.promise.service.*;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -16,9 +17,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.File;
 import java.text.DecimalFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Slf4j
@@ -32,6 +35,12 @@ public class adminHomeController {
     private final MedicationLogService medicationLogService;
     private final ReportService reportService;
 
+    private final DateTimeFormatter dtoFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private final DateTimeFormatter logFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    private final LocalDateTime startDateTime = LocalDateTime.of(2024, 10, 7, 0, 0); //관찰 시작일
+    private final LocalDateTime endDateTime = LocalDateTime.now(); // 현재 시간까지
+
     @Value("${app.log.directory:/spring-boot/log}") //app.log.directory 가 설정되지 않을 경우 기본 경로 설정
     private String LOG_DIRECTORY;
 
@@ -41,15 +50,13 @@ public class adminHomeController {
         User user = (User) session.getAttribute("user");
         if(user == null) return "redirect:/login";
 
-        // 2024년 10월 07일 00:00부터 현재까지의 범위 지정
-        LocalDateTime startDateTime = LocalDateTime.of(2024, 10, 7, 0, 0); //관찰 시작일
-        LocalDateTime endDateTime = LocalDateTime.now(); // 현재 시간까지
 
         // LocalDate로 변환하여 두 날짜의 차이를 구함
         long daysBetween = java.time.temporal.ChronoUnit.DAYS.between(startDateTime.toLocalDate(), endDateTime.toLocalDate()) + 1; // 관찰 시작일부터 1일차이기 때문에 1을 더함
 
         List<AdminHomeDTO> dtoList = new ArrayList<>();
         double totalPercent = 0.0;
+        int countUser = 0;
         DecimalFormat df = new DecimalFormat("#.##"); // 소수점 두 자리까지만 표현
         for(User u : userService.findAllUser())
         {
@@ -69,21 +76,26 @@ public class adminHomeController {
                 double percent = (logSize / (daysBetween * (double) notificationSize)) * 100;
                 newDto.setLogSize(logSize);
                 newDto.setPercent(Double.parseDouble(df.format(percent)));
-                totalPercent += newDto.getPercent();
+
+                if (newDto.getRole() != Role.admin)
+                {
+                    totalPercent += newDto.getPercent();
+                    ++countUser;
+                }
             }
             dtoList.add(newDto);
         }
-        totalPercent = Double.parseDouble(df.format((totalPercent - dtoList.get(0).getPercent() - dtoList.get(1).getPercent() - dtoList.get(2).getPercent()) / (dtoList.size() - 3))); //관리자(테스트) 계정의 복약 순응도는 반영 안함
+        totalPercent = countUser > 0 ? Double.parseDouble(df.format(totalPercent / countUser)) : 0; //관리자(테스트) 계정의 복약 순응도는 반영 안함
 
         List<reportAdminDTO> reportList = new ArrayList<>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
         for(Report r : reportService.findAllReport())
         {
             reportAdminDTO newReportDto = new reportAdminDTO();
             newReportDto.setId(r.getId());
             newReportDto.setUserName(r.getUser().getName());
             newReportDto.setTitle(r.getTitle());
-            newReportDto.setCreatAt(r.getCreateAt().format(formatter));
+            newReportDto.setCreatAt(r.getCreateAt().format(dtoFormatter));
             reportList.add(newReportDto);
         }
 
@@ -95,6 +107,14 @@ public class adminHomeController {
         {
             for (File file : logDir.listFiles()) if (file.isFile() && file.getName().startsWith("info-")) logFiles.add(file.getName());
         }
+        // 로그 파일 이름을 날짜와 순번에 따라 정렬
+        logFiles.sort(Comparator.comparing((String fileName) -> {
+            String datePart = fileName.substring(5, 15); // 날짜 부분 추출
+            return LocalDate.parse(datePart, logFormatter);
+        }).thenComparingInt(fileName -> {
+            String[] parts = fileName.split("\\."); // 순번 추출
+            return Integer.parseInt(parts[1]);
+        }));
 
         model.addAttribute("logFiles", logFiles);
         model.addAttribute("allReport", reportList);
